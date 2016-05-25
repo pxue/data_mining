3ec51b509c6ec0bf8cff3f1bf64a1aa06f4a18eb
Paul Xue <paul.xue@pressly.com>
2016-05-25 00:01:15 -0400

    presto slashcommand

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
-import os
-from flask import Flask
+import os, sys
+from flask import Flask, request
+
+from presto import get_balance
 
 PORT = int(os.environ.get("PORT", 5000))
 app = Flask(__name__)
 
-@app.route("/presto/balance")
+@app.route("/presto/balance", methods=["POST"])
 def presto_balance():
-    return "Hello World!"
+    payload = request.form.get('text')
+    user, pswd = '', ''
+    if payload:
+        user, pswd = payload.split()
+    sys.stdout.flush()
+
+    return get_balance(user, pswd)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=PORT)
